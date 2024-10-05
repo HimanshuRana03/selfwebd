59c1a32f855870e106e9e3671f37cca16484746d
HimanshuRana03 <ghsmv020406@gmail.com>
2024-10-05 10:00:58 +0530

    functions in js

---
+++ webd/jsfunctions.js
@@ -0,0 +1,23 @@
+//function add(a,b)
+//{
+  //  console.log(a+b);
+//}
+//add(2,3);
+
+//let add= function(c,d)
+//{
+  //  return (c+d);
+//}
+
+//let result=add(2,"hello");
+//console.log(result);
+
+//arrow functionns
+//bus jaisa upar likha hai wohi karna bus function hatadenge
+
+let add=(a,b)=>
+{
+    return (a+b);
+}
+let result=add(5,5);
+console.log(result);
